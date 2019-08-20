7fe12c8ce05298a0546682c9fcf72ad21a6aaace
Nate Tan <nktan93@gmail.com>
2019-08-20 20:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++) {
+    newArr = [];
+    newArr.push("Thank you, ${names[i]}, for the wonderful ${occasion} gift.")
+  }
+}
+
+function countdown() {
+  let num = 0;
+  while (num > 0) {
+    console.log(num);
+  }
+}
