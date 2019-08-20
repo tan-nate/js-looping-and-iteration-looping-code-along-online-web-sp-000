4ad3b711a4bb095c7e167b23d42060e111e3caf5
Nate Tan <nktan93@gmail.com>
2019-08-20 19:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++) {
+    newArr = [];
+    newArr.push("Thank you, ${names[i]}, for the wonderful ${occasion} gift.")
+  }
+}
